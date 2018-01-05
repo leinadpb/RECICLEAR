7f5f0b8a6955ae7e6fc2cf8cfd513427c6328d50
DANIEL JOSE PENA BERROA <1066359@est.intec.edu.do>
2018-01-05 10:31:26 -0400

    login form resized.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <br><br><br><br>
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-6 col-md-offset-3">
             <div class="panel panel-default">
                 <div class="panel-heading" align="center"><b>RECICLEAR </b>| Iniciar sesión</div>
 
