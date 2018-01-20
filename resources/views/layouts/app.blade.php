3d2a4188dcd79e6b6a47e806eeea222292cef02b
DANIEL JOSE PENA BERROA <1066359@est.intec.edu.do>
2018-01-20 08:04:08 -0400

    To heroku V1.0

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,9 +10,48 @@
 
     <title>{{ config('app.name', 'RECICLEAR') }}</title>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+     <!-- Styles -->
+    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<style type="text/css">
+.full{
+    background-color: #FF4848;
+    color:white;
+}
+.full > .caption{
+    color:white;
+}
+
+.empty{
+    background-color: #1FCB4A;
+    color:white;
+}
+.empty > .caption{
+    color:white;
+}
+
+.greaterMiddle{
+    background-color: #B6BA18;
+    color:white;
+}
+.greaterMiddle > .caption{
+    color:white;
+}
+
+.show_amount > span{
+    font-size: 2.4rem;
+}
+</style>
 </head>
 <body>
     <div id="app">
